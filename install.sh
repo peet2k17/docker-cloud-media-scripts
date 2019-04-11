1d0ea2e7677b9381917b61d704d1a96ed00b8fe9
peet2k17 <35061089+peet2k17@users.noreply.github.com>
2019-04-11 23:36:22 +0200

    Modify for rclone vfs + upgrade rclone 1.46

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Rclone variables
-_rclone_version="v1.45"
+_rclone_version="v1.46"
 rclone_release="rclone-${_rclone_version}-linux-amd64"
 rclone_zip="${rclone_release}.zip"
 rclone_url="https://github.com/ncw/rclone/releases/download/${_rclone_version}/${rclone_zip}"
 
 # Plexdrive variables
-plexdrive_bin="plexdrive-linux-amd64"
-plexdrive_url="https://github.com/dweidenfeld/plexdrive/releases/download/5.0.0/${plexdrive_bin}"
+#plexdrive_bin="plexdrive-linux-amd64"
+#plexdrive_url="https://github.com/dweidenfeld/plexdrive/releases/download/5.0.0/${plexdrive_bin}"
 
 # Rclone
 wget "$rclone_url"
@@ -18,7 +18,7 @@ rm -rf "$rclone_zip"
 rm -rf "$rclone_release"
 
 # Plexdrive
-wget "$plexdrive_url"
-chmod a+x "$plexdrive_bin"
-cp -rf "$plexdrive_bin" "/usr/bin/plexdrive"
-rm -rf "$plexdrive_bin"
+#wget "$plexdrive_url"
+#chmod a+x "$plexdrive_bin"
+#cp -rf "$plexdrive_bin" "/usr/bin/plexdrive"
+#rm -rf "$plexdrive_bin"
